99bd02fe65d27b13495783cdb62ee83e51d767b7
leopoldchan <giantchen@163.com>
2018-07-16 20:39:05 +0800

    周一运行的买入策略

--- src/test.py
+++ src/test.py
@@ -1,32 +1,31 @@
 import tushare as ts
 import easytrader
-
-# df=ts.get_hist_data('600848') #一次性获取全部数据
-# print(df[['ma10']])
-
+import time 
+from functools import reduce
 import tushare as ts
-
+import logging
+import datetime
 user = easytrader.use('ths')
 user.connect(r'D:\同花顺软件\同花顺\xiadan.exe')
 
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',  \
+                    datefmt='%a, %d %b %Y %H:%M:%S',filename="test.log",filemode='a')
+
+i=0
 while True :
-    time.sleep(1)
+    i = i+1
+    time.sleep(1.5)
     df = ts.get_realtime_quotes('601360') #Single stock symbol
     df[['code','name','price','bid','ask','volume','amount','time']]
-    print(df)
+    if((float(str(df['price'])[5:10])>=28.08) and (float(str(df['price'])[5:11])<=28.11)): #28.28  
+        break
+    logging.info("realtime %d quotes:" % i)
+    logging.info("realtime %s quotes:" % str(df))
+    # print (float(str(df['price'])[5:10]))
 
-
-
-
-str=user.balance
-print(str)
-
-re = user.sell('000735', price=12.00, amount=100)
+print("exe buy process!")
+re = user.buy('601360', price=30.52, amount=300) # 27,75*1.10=30.525  涨停价买
+logging.info("user.buy re: %s quotes:" % re)
 print(re)
 
-# re = user.buy('600460', price=12.39, amount=100)
-# print(re)
-
-# re = user.cancel_entrust('buy/sell 27')
-# print(re)
-
+# re = user.sell('000735', price=12.00, amount=100)
