e085710e5f28ee983c81eb20ffc916ce0055f2f2
leopoldchan <giantchen@163.com>
2018-07-17 23:02:49 +0800

    周二运行正常版本

--- src/AItrader_hold.py
+++ src/AItrader_hold.py
@@ -21,7 +21,7 @@ T_15H = time.strftime("%H%M%S", time.strptime("15:00:00", "%H:%M:%S"))
 
 #检测昨日买的股票，破位卖股票，主卖
 while True :
-    time.sleep(1)
+    time.sleep(1.5)
     df = ts.get_realtime_quotes('601360') #Single stock symbol
     df[['code','name','price','bid','ask','volume','amount','time']]
     TNOW = time.strftime("%H%M%S", time.strptime(str(df['time'])[5:13], "%H:%M:%S"))
@@ -33,16 +33,17 @@ while True :
     # print (float(str(df['price'])[5:10]))
     
 print("exe sell process!")
-re = user.sell('601360', price=25.15, amount=300) # 27,89*0.9=25.10  跌停价卖
-logging.info("user.sell re: %s quotes:" % re)
-print(re)
+# re = user.sell('601360', price=25.15, amount=300) # 27,89*0.9=25.10  跌停价卖
+# logging.info("user.sell re: %s quotes:" % re)
+# print(re)
 
 
 #检测今日买的股票，收盘站回卖入点，主买
 while True :
-    time.sleep(1)
+    time.sleep(1.5)
     df = ts.get_realtime_quotes('601360') #Single stock symbol
     df[['code','name','price','bid','ask','volume','amount','time']]
+    logging.info("realtime %s quotes:" % str(df))
     TNOW = time.strftime("%H%M%S", time.strptime(str(df['time'])[5:13], "%H:%M:%S"))
     if (int(TNOW)<int(T_1455H)):  #等到收盘确认
         continue
@@ -50,7 +51,7 @@ while True :
         exit()
     if (float(str(df['price'])[5:10])>=27.61) : #28.28  
         break
-    logging.info("realtime %s quotes:" % str(df))
+   
 
     # print (float(str(df['price'])[5:10]))
 
