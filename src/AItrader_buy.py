b5c7a3134ebf51b9d1da32e5908e0a9bfac6e546
chenzw-alibaba <giantchen@163.com>
2018-07-18 21:59:17 +0800

    ceshi

--- src/AItrader_buy.py
+++ src/AItrader_buy.py
@@ -6,8 +6,8 @@ import tushare as ts
 import logging
 import datetime
 
-# user = easytrader.use('ths')
-# user.connect(r'D:\同花顺软件\同花顺\xiadan.exe')
+user = easytrader.use('ths')
+user.connect(r'C:\同花顺软件\同花顺\xiadan.exe')
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',  \
                     datefmt='%a, %d %b %Y %H:%M:%S',filename="AItrader.log",filemode='a')
@@ -16,25 +16,20 @@ T_9H = time.strftime("%H%M%S", time.strptime("09:30:00", "%H:%M:%S"))
 T_11H = time.strftime("%H%M%S", time.strptime("11:30:10", "%H:%M:%S"))
 T_13H = time.strftime("%H%M%S", time.strptime("13:00:00", "%H:%M:%S"))
 T_15H = time.strftime("%H%M%S", time.strptime("15:00:00", "%H:%M:%S"))
-i=0
+
 while True :
-    i = i+1
     time.sleep(1.5)
     df = ts.get_realtime_quotes('601360') #Single stock symbol
     df[['code','name','price','bid','ask','volume','amount','time']]
-    TNOW = time.strftime("%H%M%S", time.strptime(str(df['time'])[5:13], "%H:%M:%S"))
-    if (int(TNOW)>=int(T_15H)):
-        print("time is after 15:00")
-        exit()
-    if((float(str(df['price'])[5:10])>=28.08) and (float(str(df['price'])[5:11])<=28.11)): #28.28  
-        break
-    logging.info("realtime %d quotes:" % i)
     logging.info("realtime %s quotes:" % str(df))
+    TNOW = time.strftime("%H%M%S", time.strptime(str(df['time'])[5:13], "%H:%M:%S"))
+    if (int(TNOW)<=int(T_9H)):
+        continue
+    if(float(str(df['price'])[5:10])>=27.63): #28.28  
+        break
     # print (float(str(df['price'])[5:10]))
     
 print("exe buy process!")
-# re = user.buy('601360', price=30.52, amount=300) # 27,75*1.10=30.525  涨停价买
-# logging.info("user.buy re: %s quotes:" % re)
-# print(re)
-
-# re = user.sell('000735', price=12.00, amount=100)
+re = user.buy('601360', price=30.33, amount=300) # 27,58*1.10=30.338  涨停价买
+logging.info("user.buy re: %s quotes:" % re)
+print(re)
